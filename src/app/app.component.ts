818f1d0b5886e9d3ee99dec53f097f77d1291b76
Donald Mzima <donaldmzima8@gmail.com>
2024-09-24 13:04:36 +0200

    ref: change titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TutorialViewComponent } from './tutorial-view/tutorial-view.component';
+import { ModalView, ModalViewDialog } from './modal/modal.component';
+import { FormExampleComponent } from './form-example/form-example.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ModalView,
+    ModalViewDialog,
+    TutorialViewComponent,
+    FormExampleComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Blackout';
+  title = 'Angula Cheet SheetS';
 }
